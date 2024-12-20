224477e6b9f2e878b53af3f59b7cd142e9d7fd86
rubimtech <olegsaharov@gmail.com>
2024-12-20 03:00:17 +0300

    Update setup_ssh.sh

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # curl -sSL https://raw.githubusercontent.com/rubimtech/installbot/main/setup_ssh.sh | bash
 
-
 # Путь к директории с ключами
 KEY_DIR="$HOME/.ssh"
 PRIVATE_KEY="$KEY_DIR/id_rsa"
@@ -23,18 +22,22 @@ else
     echo "Публичный ключ найден: $PUBLIC_KEY"
 fi
 
-# Добавление публичного ключа на сервер, если он еще не добавлен
+# Создание директории .ssh, если она не существует
+mkdir -p ~/.ssh
+
+# Проверка, существует ли файл authorized_keys
+if [ ! -f ~/.ssh/authorized_keys ]; then
+    echo "Файл authorized_keys не найден. Создание файла..."
+    touch ~/.ssh/authorized_keys
+    chmod 600 ~/.ssh/authorized_keys
+    echo "Файл authorized_keys создан."
+fi
+
+# Добавление публичного ключа в файл authorized_keys, если его нет
 if ! grep -q "$(cat $PUBLIC_KEY)" ~/.ssh/authorized_keys; then
     echo "Добавление публичного ключа в файл authorized_keys..."
-
-    # Создание файла authorized_keys, если его нет
-    mkdir -p ~/.ssh
-    touch ~/.ssh/authorized_keys
-
-    # Добавление публичного ключа в файл authorized_keys
     cat $PUBLIC_KEY >> ~/.ssh/authorized_keys
     chmod 600 ~/.ssh/authorized_keys
-
     echo "Публичный ключ добавлен в файл authorized_keys."
 else
     echo "Публичный ключ уже добавлен в файл authorized_keys."
@@ -42,4 +45,5 @@ fi
 
 # Вывод сообщения о завершении
 echo "Процесс завершен."
+
 
